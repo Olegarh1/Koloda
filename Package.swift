a176357ab004d9fb0e34bab7dda0be7d9176fcca
Zakladnyi <zakladnyio@gmail.com>
2025-05-20 10:02:44 +0300

    Add Pop Dependencie

--- Package.swift
+++ Package.swift
@@ -12,9 +12,15 @@ let package = Package(
             targets: ["Koloda"]
         ),
     ],
+    dependencies: [
+            .package(name: "pop", url: "https://github.com/facebook/pop.git", from: "1.0.14")
+        ],
     targets: [
         .target(
             name: "Koloda",
+            dependencies: [
+                   .product(name: "pop", package: "pop")
+               ],
             path: "Sources/Koloda"
         )
     ]
