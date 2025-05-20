d39ddecedffc0dd7b14c078505735e4a64519b02
Zakladnyi Oleg <86043017+Olegarh1@users.noreply.github.com>
2025-05-20 09:20:04 +0300

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Koloda",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "Koloda",
+            targets: ["Koloda"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "Koloda",
+            path: "Koloda"
+        )
+    ]
+)
