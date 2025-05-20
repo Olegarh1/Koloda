0ab37966f5731a2999528a2aa7d137b202d44b0c
Zakladnyi <zakladnyio@gmail.com>
2025-05-20 09:47:14 +0300

    Fix 2.0

--- Package.swift
+++ Package.swift
@@ -1,22 +1,21 @@
 // swift-tools-version:5.3
-
 import PackageDescription
 
 let package = Package(
     name: "Koloda",
     platforms: [
-        .iOS("13.0"),
+        .iOS(.v13)
     ],
     products: [
         .library(
             name: "Koloda",
             targets: ["Koloda"]
-        )
+        ),
     ],
     targets: [
         .target(
             name: "Koloda",
-            path: "Example/Koloda",
+            path: "Sources/Koloda"
         )
     ]
 )
